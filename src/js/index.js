6aeee3caa8b008082280d34d2028fa20d7e534aa
xabi1000 <babelwado@gmail.com>
2021-10-04 21:11:44 +0100

    completed project

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 const card = document.querySelectorAll('.card');
 const cardArrow = document.getElementById('card__arrow');
 
-// console.log(container);
-// console.log(container[0].dataset.diretion);
-// console.log(container[1]);
- console.log(card[0]);
+console.log(card[0].classList);
 
-const translateCard = translate => {
-    console.log(translate)
+const slideCard = translate => {
+    if (translate === 'next'){
+        card[0].classList.add('ToLeft');
+        card[1].classList.add('ToLeft');
+    }else{
+        card[0].classList.remove('ToLeft');
+        card[1].classList.remove('ToLeft');   
 }
 
-cardArrow.addEventListener('click', (e) =>{
-    console.log(e)
-    // translateCard(e.target.dataset.diretion) 
-});
+}
+ cardArrow.addEventListener('click', (e) =>{
+    slideCard(e.target.dataset.direction); 
+ });
